2f67ab2551ab2c0caa05c36f343b811648451bc7
faizan-multani <faizan.multani60@gmail.com>
2025-02-24 12:33:09 +0530

    first commit

---
+++ Theme/Theme.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll(".btn")
+const body = document.querySelector("body")
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id
+      body.style.color = "black"
+    }
+    if (event.target.id === "black") {
+      body.style.backgroundColor = event.target.id
+      body.style.color = "white"
+    }
+  })
+})
